af3a3952c8f65d36b80dd1d4038eece63dc80939
Richard.Hu <hsl200909@163.com>
2018-09-27 22:37:07 +0800

    feat(traffic): 新增最近一月的浏览统计显示功能。

--- HslHomepage/Pages/Index.cshtml.cs
+++ HslHomepage/Pages/Index.cshtml.cs
@@ -9,6 +9,11 @@ namespace HslHomepage.Pages
 {
     public class IndexModel : PageModel
     {
+        public string X = "";
+        public string Y = "";
+        public List<string> x_text = new List<string>( );
+        public List<int> y_count = new List<int>( );
+
         public void OnGet()
         {
             HslHomepage.Data.StaticInfomation.AccessCount++;
@@ -19,6 +24,35 @@ namespace HslHomepage.Pages
                 ushort value = BitConverter.ToUInt16( Data.StaticInfomation.Tmp, i * 2 );
                 value++;
                 BitConverter.GetBytes( value ).CopyTo( Data.StaticInfomation.Tmp, i * 2 );
+
+                int startIndex = i;
+                for (int j = 0; j < 30; j++)
+                {
+                    x_text.Add( DateTime.Now.Date.AddDays( j - 29 ).Day.ToString( ) + "日" );
+                    if (j < 29 - startIndex)
+                    {
+                        y_count.Add( 0 );
+                    }
+                    else
+                    {
+                        y_count.Add( BitConverter.ToUInt16( Data.StaticInfomation.Tmp, (startIndex + j - 29) * 2 ) );
+                    }
+                }
+
+                for (int j = 0; j < 30; j++)
+                {
+                    if(j == 29)
+                    {
+                        X += "'" + x_text[j] + "'";
+                        Y += y_count[j].ToString( );
+                    }
+                    else
+                    {
+                        X += "'" + x_text[j] + "',";
+                        Y += y_count[j].ToString( ) + ",";
+                    }
+                }
+
 
                 System.IO.File.WriteAllBytes( "Tmp.txt", Data.StaticInfomation.Tmp );
             }
