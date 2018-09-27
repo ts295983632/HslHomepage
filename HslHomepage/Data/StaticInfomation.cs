29bd6612c4ce3751154bab39f0af85e879963bda
dathlin <hsl200909@163.com>
2018-09-27 17:34:51 +0800

    feat(date): 自动记录每天的访问量，方便统计显示

--- HslHomepage/Data/StaticInfomation.cs
+++ HslHomepage/Data/StaticInfomation.cs
@@ -9,11 +9,20 @@ namespace HslHomepage.Data
     {
         static StaticInfomation( )
         {
-            
+            if (System.IO.File.Exists( "Tmp.txt" ))
+            {
+                Tmp = System.IO.File.ReadAllBytes( "Tmp.txt" );
+            }
+            else
+            {
+                System.IO.File.WriteAllBytes( "Tmp.txt", Tmp );
+            }
         }
 
         public static int AccessCount { get; set; } = 0;
 
+        public static DateTime start = new DateTime( 2018, 9, 27 );
+        public static byte[] Tmp = new byte[10000];
 
     }
 }
--- HslHomepage/Pages/Index.cshtml.cs
+++ HslHomepage/Pages/Index.cshtml.cs
@@ -12,6 +12,17 @@ namespace HslHomepage.Pages
         public void OnGet()
         {
             HslHomepage.Data.StaticInfomation.AccessCount++;
+
+            int i = (int)(DateTime.Now.Date - Data.StaticInfomation.start).TotalDays;
+            lock (Data.StaticInfomation.Tmp)
+            {
+                ushort value = BitConverter.ToUInt16( Data.StaticInfomation.Tmp, i * 2 );
+                value++;
+                BitConverter.GetBytes( value ).CopyTo( Data.StaticInfomation.Tmp, i * 2 );
+
+                System.IO.File.WriteAllBytes( "Tmp.txt", Data.StaticInfomation.Tmp );
+            }
+
             Utils.SoftUtils.NumericalOrder.GetLongOrder( );
         }
     }
